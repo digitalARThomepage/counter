b9b2e47be139ab0a03f9987ef0610e64875fb712
digitalARThomepage <digitalarthomepage@gmail.com>
2021-11-20 10:06:58 +0100

    2.0

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-function countdown(dateEnd) {
-      var timer, days, hours, minutes, seconds;
-      dateEnd = new Date(dateEnd);
-      dateEnd = dateEnd.getTime();
-      if ( isNaN(dateEnd) ) {
-        return;
-      }
-      timer = setInterval(calculate, 1000);
-      function calculate() {
-        var dateStart = new Date();
-        var dateStart = new Date(dateStart.getUTCFullYear(),
-                             dateStart.getUTCMonth(),
-                             dateStart.getUTCDate(),
-                             dateStart.getUTCHours(),
-                             dateStart.getUTCMinutes(),
-                             dateStart.getUTCSeconds());
-        var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+(() => {
+  // Specify the deadline date
+  const deadlineDate = new Date('December 1, 2021 12:00:00').getTime();
 
-        if ( timeRemaining >= 0 ) {
-          days    = parseInt(timeRemaining / 86400);
-          timeRemaining   = (timeRemaining % 86400);
-          hours   = parseInt(timeRemaining / 3600);
-          timeRemaining   = (timeRemaining % 3600);
-          minutes = parseInt(timeRemaining / 60);
-          timeRemaining   = (timeRemaining % 60);
-          seconds = parseInt(timeRemaining);
+  // Cache all countdown boxes into consts
+  const countdownDays = document.querySelector('.countdown__days .number');
+  const countdownHours = document.querySelector('.countdown__hours .number');
+  const countdownMinutes = document.querySelector('.countdown__minutes .number');
+  const countdownSeconds = document.querySelector('.countdown__seconds .number');
 
-          $("#c_days").text(parseInt(days, 10));
-          $("#c_hours").text(("0" + hours).slice(-2));
-          $("#c_mins").text(("0" + minutes).slice(-2));
-          $("#c_sec").text(("0" + seconds).slice(-2));
-        } else {
-          return;
-        }
-      }
-      function display(days, hours, minutes, seconds) {}
-    }
-    countdown('12/31/2019 12:00:00 AM');
+  // Update the count down every 1 second (1000 milliseconds)
+  setInterval(() => {
+    // Get current date and time
+    const currentDate = new Date().getTime();
+
+    // Calculate the distance between current date and time and the deadline date and time
+    const distance = deadlineDate - currentDate;
+
+    // Calculations the data for remaining days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    const seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+    // Insert the result data into individual countdown boxes
+    countdownDays.innerHTML = days;
+    countdownHours.innerHTML = hours;
+    countdownMinutes.innerHTML = minutes;
+    countdownSeconds.innerHTML = seconds;
+  }, 1000);
+})();
