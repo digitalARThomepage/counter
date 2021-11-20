5ce1f4157e839b40cd696d381ae8e6f73d411799
digitalARThomepage <digitalarthomepage@gmail.com>
2021-11-20 11:04:13 +0100

    2.3

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ const fx = new TextScramble(el)
 let counter = 0
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 2000)
+    setTimeout(next, 1000)
   })
   counter = (counter + 1) % phrases.length
 }
