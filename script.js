70758186fc0317231abfef8bd3493fbede17cda1
digitalARThomepage <digitalarthomepage@gmail.com>
2021-11-20 16:20:09 +0100

    1.0

--- script.js
+++ script.js
@@ -94,9 +94,9 @@ class TextScramble {
 
 const phrases = [
   'digital<b>ART</b>homepage launch is right around the corner',
-  'Add your NFT to eternal ARTboard',
-  'The largest NFT auction potential',
   'Take part in a historic event',
+  'Add your NFT to eternal <b>ART</b>board',
+  'The largest NFT auction potential',
   'Become part of the DAO NFT project',
   'Show your masterpiece to everyone, forever',
   'Based on smartcontract, stored on blockchain',
